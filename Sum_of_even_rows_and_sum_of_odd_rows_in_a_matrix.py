8c91c708fefeea4e8bd9f94b3640516a679e8696
22A91A04M3 <119720475+22A91A04M3@users.noreply.github.com>
2023-11-09 16:00:09 +0530

    r,c = map(int,input().split()) mat = [] for innerlist in range(r):     innerlist = list(map(int,input().split()))     mat.append(innerlist) s=0 s1=0 for i in range(r):     for j in range(c):         if i%2==0:             s=s+mat[i][j]         else:             s1=s1+mat[i][j] print(s,s1)

--- Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.py
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.py
@@ -1,15 +1,14 @@
-r,c=map(int,input().split())
-mat=[]
-for inner_list in range(r):
-    inner_list = list(map(int,input().split()))
-    mat.append(inner_list)
+r,c = map(int,input().split())
+mat = []
+for innerlist in range(r):
+    innerlist = list(map(int,input().split()))
+    mat.append(innerlist)
+s=0
 s1=0
-s2=0
 for i in range(r):
     for j in range(c):
         if i%2==0:
-            s1+=mat[i][j]
+            s=s+mat[i][j]
         else:
-            s2+=mat[i][j]
-print(s1, end= ' ')
-print(s2)
+            s1=s1+mat[i][j]
+print(s,s1)
